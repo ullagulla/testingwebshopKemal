444a640eac5ba919c119d9e8163f775e1af90c5a
ullagulla <ulrikaa91@hotmail.com>
2020-02-26 13:36:50 +0100

    ändringar med databaseurl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 const { app, port } = require('./src/server')
-const dbConfig = require('./config/config').databaseURL
+const dbConfig = process.env.MONGO_ATLAS_URL
 
 
 // Kicka igång servern
